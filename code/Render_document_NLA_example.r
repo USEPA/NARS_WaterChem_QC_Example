6b2e8fe79b05bd9065812f548d66bcc0e11d9e88
Blocksom <Blocksom.Karen@epa.gov>
2025-04-25 09:16:22 -0700

    Updated code and files to change QA to QC and report to output

--- code/Render_document_NLA_example.r
+++ code/Render_document_NLA_example.r
@@ -1,14 +1,14 @@
 library(quarto)
 
-outDir <- paste0(here::here(), "/reports/Example_NLA_Water_Chem_QA_Report_", Sys.Date())
+outDir <- paste0(here::here(), "/output/Example_NLA_Water_Chem_QC_Output_", Sys.Date())
 dir.create(path = outDir, showWarnings = FALSE, recursive = TRUE)
 
 inDir_raw <- paste0(here::here(), "/data") 
 inDir_data <- paste0(here::here(), "/data") 
-fileout <- paste0("Example_NLA_Water_Chem_QA_Report_", Sys.Date(), ".html")
+fileout <- paste0("Example_NLA_Water_Chem_QC_Output_", Sys.Date(), ".html")
 
 setwd("./code")
-quarto_render(paste0(here::here(), "/code/NLA_QA_document.qmd"), 
+quarto_render(paste0(here::here(), "/code/NLA_QC_document.qmd"), 
               output_file = fileout, 
               output_format = 'html',
               execute_params = list(outDir = outDir,
